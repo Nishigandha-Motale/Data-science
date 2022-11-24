9719089a4b419fd8000ca6af59625f54064d40d8
Nishigandha-Motale <111187376+Nishigandha-Motale@users.noreply.github.com>
2022-11-24 13:15:28 +0530

    Add files via upload

---
+++ Joins in R.R
@@ -0,0 +1,94 @@
+# Join (Merge) data frames (inner, outer, left, right) 
+df1 = data.frame(CustomerId = c(1:6), Product = c(rep("Oven", 3), rep("Television", 3)))
+df1
+df2 = data.frame(CustomerId = c(2, 4, 6,7), State = c(rep("California", 3), rep("Texas", 1)))
+df2
+View(df2)
+
+# Inner Join
+df<-merge(x=df1,y=df2,by="CustomerId")
+df
+# Outer Join
+df<-merge(x=df1,y=df2,by="CustomerId",all=TRUE)
+df
+x<-c(8,'Oven','California') # cbind is used to combine column wise 
+df3<-rbind(df,x) # it is used to combine rows in data frame
+df3
+# Left outer join
+df<-merge(x=df1,y=df2,by="CustomerId",all.x=TRUE)
+df
+# Right outer join 
+df<-merge(x=df1,y=df2,by="CustomerId",all.y=TRUE)
+df
+# Cross join
+df<-merge(x = df1, y = df2, by = NULL)
+df
+
+
+data("iris")
+View(iris)
+
+# Apply functions 
+# Returns a vector or array or list of values obtained by 
+#applying a function to margins of an array or matrix or data frames 
+#apply(df , 1 or 2 , function)
+
+z = apply(iris[,1:4],2,mean)
+z
+# # lapply function takes list, vector or Data frame  as input and returns only list as output
+#lapply(df , function)
+x = lapply(iris[,1:4],mean)
+x
+# # sapply function takes list, vector or Data frame  as input. It is similar to lapply 
+#function but returns only vector as output
+
+y = sapply(iris[,1:4],mean)
+y
+
+
+data("iris")
+iris$Sepal.Length
+
+attach(iris)
+Sepal.Length
+
+# tapply
+
+mean(iris$Sepal.Length)
+
+
+data("mtcars")
+View(mtcars)
+mtcars$mpg
+
+
+nrow(mtcars) # no.of rows in mtcars
+row.names(mtcars) # row names 
+ncol(mtcars) # number of columns 
+colnames(mtcars) # column names in mtcars 
+names(mtcars) 
+dim(mtcars) # dimensions ( rows X columns )
+dimnames(mtcars) # Dimension names ( row names and column names )
+
+head(mtcars,10) # top 10 observations 
+tail(mtcars) # bottom 10 observations
+
+data("mtcars")
+min(mtcars$mpg, na.rm = T) # na.rm --> Remove NA values 
+max(mtcars$mpg, na.rm = T)
+range(mtcars$mpg,na.rm = T) # Return both min and max 
+
+# mean, median
+mean(mtcars$mpg, na.rm = T) # Average 
+median(mtcars$mpg, na.rm = T) # middle most value in data after sorting in ascending or descending
+range(mtcars$mpg,na.rm = T)
+mode(mtcars$hp)
+
+library(modeest)
+mlv(mtcars$hp, method = "mfv")
+
+x = factor(mtcars$cyl)
+x
+table(x)
+
+
